defceeccd70e46a62e82f5cf67b9b08c38218ba2
mbkkt <valera.mironow@gmail.com>
2019-01-20 11:51:31 +0300

    New

--- app.py
+++ app.py
@@ -1,22 +1,13 @@
-from flask import Flask
-
 from bot import run
 
 
 def app_run():
-    try:
-        run.run()
-    except Exception as e:
-        with open('data/log.txt', 'a') as log_file:
-            print(e)
-            log_file.write(str(e) + '\n\n')
+    while True:
+        try:
+            run.run()
+        except Exception as e:
+            with open('/home/MBkkt/VkSupremeGroupBot/data/log.txt', 'a') as log_file:
+                log_file.write(str(e) + '\n\n')
 
 
 app_run()
-
-app = Flask(__name__)
-
-
-@app.route('/', methods=('GET', 'POST'))
-def about_me():
-    return "Bot by MBkkt"
